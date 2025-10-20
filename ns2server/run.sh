f52b99c4d5f0d52f0c4a8bfa8a96cdca2f833136
adsfgg <11364579+adsfgg@users.noreply.github.com>
2025-10-20 21:58:22 +0100

    feat(ns2-server): pass arguments to script, fetch bucket name from ssm

--- ns2server/run.sh
+++ ns2server/run.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+NAME="$1"
+MAP="$2"
+PASSWORD="$3"
+LAUNCH_CONFIG="$4"
+
+echo "Starting server"
+
+# Fetch config from s3 (mods, configs, etc.)
+BUCKET_NAME="$(aws ssm get-parameter --name "/NS2Arena/ConfigBucket/Name")"
+aws s3 sync s3://$BUCKET_NAME/$LAUNCH_CONFIG /server
+PLAYER_LIMIT="$(cat /server/config.json | .PlayerLimit)"
+SPEC_LIMIT="$(cat /server/config.json | .SpecLimit)"
+
 _term() {
   echo "Caught SIGTERM, killing server"
   kill -TERM "$child" 2>/dev/null
 }
 
 trap _term SIGTERM
-
-echo "Starting server"
-
-# Fetch config from s3 (mods, configs, etc.)
-aws s3 sync $S3_BASE/launch_configs/$LAUNCH_CONFIG /server
-PLAYER_LIMIT="$(cat /server/config.json | .PlayerLimit)"
-SPEC_LIMIT="$(cat /server/config.json | .SpecLimit)"
 
 # /gamedata/x64/server_linux -file /server/config.txt &
 /gamedata/x64/server_linux \
