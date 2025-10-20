a31d164a4afe19f2ac536fe3fa46ba820322cef7
adsfgg <11364579+adsfgg@users.noreply.github.com>
2025-10-20 21:07:27 +0100

    feat(ns2-server): make container configurable through environment variables

--- ns2server/run.sh
+++ ns2server/run.sh
@@ -8,7 +8,24 @@ _term() {
 trap _term SIGTERM
 
 echo "Starting server"
-/gamedata/x64/server_linux -file /server/config.txt &
+
+# Fetch config from s3 (mods, configs, etc.)
+aws s3 sync $S3_BASE/launch_configs/$LAUNCH_CONFIG /server
+PLAYER_LIMIT="$(cat /server/config.json | .PlayerLimit)"
+SPEC_LIMIT="$(cat /server/config.json | .SpecLimit)"
+
+# /gamedata/x64/server_linux -file /server/config.txt &
+/gamedata/x64/server_linux \
+  -limit $PLAYER_LIMIT \
+  -speclimit $SPEC_LIMIT \
+  -password $PASSWORD \
+  -name $NAME \
+  -config_path /server/configs \
+  -logdir /server/logs \
+  -modstorage /server/modstore \
+  -port 27015 \
+  -map $MAP \
+  -startmodserver
 child="$!"
 wait "$child"
 
